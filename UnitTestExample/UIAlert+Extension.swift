489c6aaf332394dba67f7c8de12fc7c0d8677502
keerthiv <keerthi@sparkouttech.com>
2020-06-30 13:49:24 +0530

    UIAlert added

--- UnitTestExample/UIAlert+Extension.swift
+++ UnitTestExample/UIAlert+Extension.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Hxtreme. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIAlertController {
+    static func showSimpleAlert(_ vc: UIViewController, title: String?, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+        }))
+        vc.present(alertController, animated: true, completion: nil)
+    }
+}
